3a02f7223be12bb37ee3ce1864cb20fc69a0755c
Moin Syed <moinn5810@gmail.com>
2023-09-15 20:26:23 -0400

    Adding author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Moin Syed <moinn5810@gmail.com>
 #include<iostream>  
   
 using namespace std; 
